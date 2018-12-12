632e33ae6129c499c9775a84151e2db6f623dd7d
Cui <cui_cjm@163.com>
2018-12-12 17:39:12 +0800

    css commit

--- src/main/scala/com/pharbers/pptxmoudles/PhExcelCss.scala
+++ src/main/scala/com/pharbers/pptxmoudles/PhExcelCss.scala
@@ -1,5 +1,17 @@
 package com.pharbers.pptxmoudles
 
-class PhExcelCss {
+import com.pharbers.macros.api.commonEntity
 
+class PhExcelCss extends commonEntity{
+    var factory = "com.pharbers.process.stm.step.pptx.slider.content.phReportContentImpl"
+    var cell = ""
+    var fontSize = ""
+    var fontColor = ""
+    var fontName = ""
+    var fontStyle: List[String] = Nil
+    var cellColor = ""
+    var cellBorders: List[String] = Nil
+    var cellBordersColor = "#CDFFFF"
+    var width = "0"
+    var height = "0"
 }
