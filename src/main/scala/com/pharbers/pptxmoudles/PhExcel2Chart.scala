7228c2da93305f5a7fb48ca512a8bce5e9241e67
Cui <cui_cjm@163.com>
2018-12-24 15:33:39 +0800

    bule table

---
+++ src/main/scala/com/pharbers/pptxmoudles/PhExcel2Chart.scala
@@ -0,0 +1,11 @@
+package com.pharbers.pptxmoudles
+
+import com.pharbers.macros.api.commonEntity
+
+class PhExcel2Chart extends commonEntity{
+    var name: String = ""
+    var pos: List[Int] = List()
+    var slider: Int = 0
+    var chartType = ""
+    var css = ""
+}
--- src/main/scala/com/pharbers/pptxmoudles/PhRequest.scala
+++ src/main/scala/com/pharbers/pptxmoudles/PhRequest.scala
@@ -8,6 +8,7 @@ import com.pharbers.macros.common.connecting.{One2OneConn, ToStringMacro}
 @One2OneConn[PhExcel2PPT]("e2p")
 @One2OneConn[PhExportPPT]("exp")
 @One2OneConn[PhTextPPT]("text")
+@One2OneConn[PhExcel2Chart]("e2c")
 class PhRequest() extends commonEntity{
     var jobid: String =""
     var command: String = ""
