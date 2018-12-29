4b3d099af9b4060d8dc90aef212a3669e7fecd27
dcs <419787760@qq.com>
2018-12-29 20:32:17 +0800

    批量朴实、

--- src/main/scala/com/pharbers/pptxmoudles/PhExcelPush.scala
+++ src/main/scala/com/pharbers/pptxmoudles/PhExcelPush.scala
@@ -3,10 +3,8 @@ package com.pharbers.pptxmoudles
 import com.pharbers.macros.api.commonEntity
 import com.pharbers.macros.common.connecting.One2OneConn
 
-@One2OneConn[PhExcelCss]("css")
 class PhExcelPush() extends commonEntity{
     var name: String = ""
-    var cell: String = ""
-    var cate: String = ""
-    var value: String = ""
+    //-c#cell-s#style-v#value
+    var cells: List[String] = Nil
 }
